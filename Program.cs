e8daf39dd890fa403123366084ac24bb30572854
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-17 14:42:18 -0500

    [1.16] Option 4 - Display categories and related products - Enforce eager loading

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ try
         Console.WriteLine("1) Display Categories");
         Console.WriteLine("2) Add Category");
         Console.WriteLine("3) Display Category and related products");
+        Console.WriteLine("4) Display all Categories and their related products");
         Console.WriteLine("\"q\" to quit");
         choice = Console.ReadLine();
         Console.Clear();
@@ -91,6 +92,18 @@ try
             foreach (Product p in category.Products)
             {
                 Console.WriteLine($"\t{p.ProductName}");
+            }
+        }
+                else if (choice == "4")
+        {
+            var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.CategoryName}");
+                foreach (Product p in item.Products)
+                {
+                    Console.WriteLine($"\t{p.ProductName}");
+                }
             }
         }
         Console.WriteLine();
