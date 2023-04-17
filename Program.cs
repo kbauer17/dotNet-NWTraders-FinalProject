9424ea46473d6642b56c305cc21706524feeb53c
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-17 14:23:27 -0500

    [1.08] Option 1 - Display Categories

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ logger.Info("Program started");
 
 try
 {
+    var db = new NWConsole_23_kjbContext();
     string choice;
     do
     {
@@ -19,6 +20,21 @@ try
         Console.WriteLine("\"q\" to quit");
         choice = Console.ReadLine();
         Console.Clear();
+        logger.Info($"Option {choice} selected");
+        if (choice == "1")
+        {
+            var query = db.Categories.OrderBy(p => p.CategoryName);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{query.Count()} records returned");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.CategoryName} - {item.Description}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        Console.WriteLine();
 
     } while (choice.ToLower() != "q");
 }
