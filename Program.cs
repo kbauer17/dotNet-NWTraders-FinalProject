b325884a4fcf244e8d4fa9257a9909d4d5165e4e
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-17 14:33:08 -0500

    [1.12] Custom validation - Check for unique CategoryName

--- Program.cs
+++ Program.cs
@@ -49,8 +49,19 @@ try
             var isValid = Validator.TryValidateObject(category, context, results, true);
             if (isValid)
             {
-                logger.Info("Validation passed");
-                // TODO: save category to db
+                //logger.Info("Validation passed");
+                // check for unique name
+                if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
+                {
+                    // generate validation error
+                    isValid = false;
+                    results.Add(new ValidationResult("Name exists", new string[] { "CategoryName" }));
+                }
+                else
+                {
+                    logger.Info("Validation passed");
+                    // TODO: save category to db
+                }
             }
             if (!isValid)
             {
