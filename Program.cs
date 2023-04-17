d26966951ea2df0042dbaaba4990581d93072c79
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-17 14:19:53 -0500

    [1.07] Program menu - Display user options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using NLog;
+using System.Linq;
+using Northwind_console.Model;
 
 // See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + "\\nlog.config";
@@ -7,6 +9,22 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+try
+{
+    string choice;
+    do
+    {
+        Console.WriteLine("1) Display Categories");
+        Console.WriteLine("2) Add Category");
+        Console.WriteLine("\"q\" to quit");
+        choice = Console.ReadLine();
+        Console.Clear();
+
+    } while (choice.ToLower() != "q");
+}
+catch (Exception ex)
+{
+    logger.Error(ex.Message);
+}
 
 logger.Info("Program ended");
